ee9efeef57990d46fae844a890369bb9f7815bb1
Aarya ChaudharyE <aaryacdy7276@gmail.com>
2022-04-06 22:59:38 +0545

    basic javascript practive

---
+++ fun2.java
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>function call pass </title>
+<script language="javascript">
+function sum(n1,n2)
+{
+var ans=n1+n2
+document.write(the sum is "+ans)
+}
+</head>
+<body>
+<h1 align="center">FUNCTION CALL PASSING PARAMETERS </h1>
+<script language="javascript">
+sum(5,6)
+</script>
+</html>
